f3cf239ea6e81647affa2ceaab267c1327188f6f
Matthieu <matthieu.falce@inria.fr>
2014-07-08 17:18:24 +0200

    added simple controller class. Note the #include main.moc !

--- code/qml_cpp_interaction/main.cpp
+++ code/qml_cpp_interaction/main.cpp
@@ -1,5 +1,24 @@
 #include <QtGui/QGuiApplication>
 #include "qtquick2applicationviewer.h"
+#include <QObject>
+#include <iostream>
+
+#include <QQuickItem>
+#include <QQuickView>
+
+
+class controller : public QObject
+{
+    Q_OBJECT
+
+public slots:
+    void cppSlot(double nb){std::cout << "from cpp : " << nb;}
+
+};
+
+//needed for inline class definitions...
+//otherwise, strange vtable errors pop up...
+#include "main.moc"
 
 int main(int argc, char *argv[])
 {
